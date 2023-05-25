f78ec9483b6e9b6d873f6041434f5a5af67d43ff
androidblog <androidblog@yandex.ru>
2023-05-25 19:20:52 +0400

    create board list component and add to left panel

---
+++ src/components/BoardList.tsx
@@ -0,0 +1,32 @@
+import BoardListItem from "./BoardListItem";
+const boads = [
+  {
+    title: "Platform Launch",
+    selected: true,
+  },
+  {
+    title: "Marketing Plan",
+    selected: false,
+  },
+  {
+    title: "Roadmap",
+    selected: false,
+  },
+];
+
+export default function BoardList() {
+  return (
+    <div className="flex flex-col">
+      <h2 className="pl-8 text-heading-s uppercase text-medium-gray">
+        ALL BOARDS ({boads.length})
+      </h2>
+      {boads.map((board) => (
+        <BoardListItem
+          key={board.title}
+          title={board.title}
+          selected={board.selected}
+        />
+      ))}
+    </div>
+  );
+}
--- src/components/LeftsidePanel.tsx
+++ src/components/LeftsidePanel.tsx
@@ -1,10 +1,12 @@
 import { Logo } from "@/components/Logo";
 import { ThemeToggleSwitch } from "./ThemeToggleSwitch";
+import BoardList from "./BoardList";
 
 export default function LeftsidePanel() {
   return (
     <div className="col-start-1 col-end-2 row-start-1 row-end-3 border-r border-lines-light bg-white dark:border-lines-dark dark:bg-dark-gray">
       <Logo />
+      <BoardList />
       <ThemeToggleSwitch />
     </div>
   );
