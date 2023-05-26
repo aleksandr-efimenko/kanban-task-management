e10f6ead55565e1ef42c2961c79fad14741cf45d
androidblog <androidblog@yandex.ru>
2023-05-26 17:46:30 +0400

    create hide panel button

---
+++ src/components/HidePanelButton.tsx
@@ -0,0 +1,20 @@
+import hideIcon from "~/assets/icon-hide-sidebar.svg";
+import Image, { type StaticImageData } from "next/image";
+import Link from "next/link";
+
+export default function HidePanelButton() {
+  return (
+    <Link
+      href="javascript:void(0)"
+      className="flex min-h-[3rem] items-center gap-[0.9375rem] pl-8"
+    >
+      <Image
+        src={hideIcon as StaticImageData}
+        alt="Hide sidebar icon"
+        width={18}
+        height={16}
+      />
+      <p className="text-medium-gray">Hide Sidebar</p>
+    </Link>
+  );
+}
