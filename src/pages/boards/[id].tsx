6a34987b04f9e7c6ab686cc6462cbe13416dce61
androidblog <androidblog@yandex.ru>
2023-05-27 18:53:14 +0400

    create board page with routing through board id

---
+++ src/pages/boards/[id].tsx
@@ -0,0 +1,47 @@
+import { useRouter } from "next/router";
+import { TaskColumn } from "@/components/TaskColumn";
+import { type Column } from "@/utils/DataTypes";
+import { NewColumnButton } from "@/components/NewColumnButton";
+import { useContext } from "react";
+import { BoardsContext } from "@/context/BoardsContext";
+import { BoardViewContainer } from "@/components/BoardView";
+import Head from "next/head";
+
+export default function BoardView() {
+  const router = useRouter();
+  const { boards } = useContext(BoardsContext);
+  const currentBoard = boards.find((board) => board.id === router.query.id);
+  if (!currentBoard) {
+    return (
+      <BoardViewContainer>
+        <h1 className="text-heading-xl text-medium-gray">Board not found</h1>
+      </BoardViewContainer>
+    );
+  }
+
+  const columns = currentBoard.columns;
+  if (!router.query.id) {
+    return (
+      <BoardViewContainer>
+        <h1 className="text-heading-xl text-medium-gray">No board selected</h1>
+      </BoardViewContainer>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Kanban task management web app | {currentBoard.name}</title>
+        <meta name="description" content="Kanban task management web app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <BoardViewContainer>
+        {columns &&
+          columns.map((column) => (
+            <TaskColumn key={column.name} column={column} />
+          ))}
+        <NewColumnButton />
+      </BoardViewContainer>
+    </>
+  );
+}
