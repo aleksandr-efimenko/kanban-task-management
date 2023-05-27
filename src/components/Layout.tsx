4e0272a54249d49e6868998000d7fd53b23fa9b5
androidblog <androidblog@yandex.ru>
2023-05-27 18:54:59 +0400

    create and use layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import { LeftsidePanel } from "./LeftsidePanel";
+import { TopPanel } from "./TopPanel";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+});
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <main
+        className={`${plusJakartaSans.className} grid h-screen w-full grid-cols-main-layout grid-rows-main-layout overflow-hidden`}
+      >
+        <LeftsidePanel />
+        {children}
+        <TopPanel />
+      </main>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { Plus_Jakarta_Sans } from "next/font/google";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
-
-const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  weight: ["500", "700"],
-});
+import Layout from "@/components/Layout";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -19,9 +14,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class">
-        <div className={plusJakartaSans.className}>
+        <Layout>
           <Component {...pageProps} />
-        </div>
+        </Layout>
       </ThemeProvider>
     </SessionProvider>
   );
