edab467f82385a0a5ac8810130d3b258d37dce45
androidblog <androidblog@yandex.ru>
2023-05-27 18:55:43 +0400

    separate board view from entire board

--- src/components/BoardView.tsx
+++ src/components/BoardView.tsx
@@ -1,10 +1,8 @@
-import boardData from "@/data/defaultBoard.json";
-import { TaskColumn } from "@/components/TaskColumn";
-import { type Column } from "@/utils/DataTypes";
-import { NewColumnButton } from "./NewColumnButton";
-
-export function BoardView() {
-  const columns: Column[] = boardData.boards[0].columns;
+export function BoardViewContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div
       className="col-start-2 col-end-3 row-start-2 row-end-3
@@ -13,10 +11,7 @@ export function BoardView() {
         bg-light-gray p-6 dark:bg-very-dark-gray
     "
     >
-      {columns.map((column) => (
-        <TaskColumn key={column.name} column={column} />
-      ))}
-      <NewColumnButton />
+      {children}
     </div>
   );
 }
