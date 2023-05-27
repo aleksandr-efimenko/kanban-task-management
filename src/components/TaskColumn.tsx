ea44d0ff80d9bcbb502505ecf751a45bc71c746c
androidblog <androidblog@yandex.ru>
2023-05-27 10:18:11 +0400

    create task column component

---
+++ src/components/TaskColumn.tsx
@@ -0,0 +1,16 @@
+import { type Column } from "@/utils/DataTypes";
+import { TaskCard } from "@/components/TaskCard";
+
+export function TaskColumn({ column }: { column: Column }) {
+  return (
+    <div className="flex flex-col gap-5 overflow-y-auto">
+      {column.tasks.map((task) => (
+        <TaskCard
+          key={task.title}
+          title={task.title}
+          subtasks={task.subtasks}
+        />
+      ))}
+    </div>
+  );
+}
