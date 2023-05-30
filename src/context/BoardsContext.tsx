fcf2d878fe1ad80ed9809a0a58749f4d86039bcf
androidblog <androidblog@yandex.ru>
2023-05-30 14:23:45 +0400

    add action kinds to CUD objects

--- src/context/BoardsContext.tsx
+++ src/context/BoardsContext.tsx
@@ -1,27 +1,40 @@
-import { createContext, useContext, useReducer } from "react";
-
+import { createContext, useContext, useReducer, type Dispatch } from "react";
 import defaultBoardsData from "@/data/defaultBoard.json";
 import { type Board } from "@/utils/DataTypes";
+import { uuid } from "uuidv4";
 
 const initialBoards = defaultBoardsData.boards;
 
-const BoardsContext = createContext(initialBoards);
-const BoardsDispatchContext = createContext(null);
+const BoardsContext = createContext<Board[] | null>(null);
+const BoardsDispatchContext = createContext<Dispatch<BoardAction> | null>(null);
 
 // An enum with all the types of actions to use in our reducer
-enum ActionKind {
-  ADD = "ADD",
-  CHANGE = "CHANGE",
-  DELETE = "DELETE",
+export enum ActionKind {
+  ADD_BOARD = "ADD_BOARD",
+  CHANGE_BOARD = "CHANGE_BOARD",
+  DELETE_BOARD = "DELETE_BOARD",
+  ADD_COLUMN = "ADD_COLUMN",
+  CHANGE_COLUMN = "CHANGE_COLUMN",
+  DELETE_COLUMN = "DELETE_COLUMN",
+  ADD_TASK = "ADD_TASK",
+  CHANGE_TASK = "CHANGE_TASK",
+  DELETE_TASK = "DELETE_TASK",
+  ADD_SUBTASK = "ADD_SUBTASK",
+  CHANGE_SUBTASK = "CHANGE_SUBTASK",
+  DELETE_SUBTASK = "DELETE_SUBTASK",
 }
 
 // An interface for our actions
 interface BoardAction {
   type: ActionKind;
-  payload: {
-    id: string;
-    name?: string;
-  };
+  boardId?: string;
+  boardName?: string;
+  columnId?: string;
+  columnName?: string;
+  taskId?: string;
+  taskName?: string;
+  subtaskId?: string;
+  subtaskName?: string;
 }
 
 export function BoardsProvider({ children }: { children: React.ReactNode }) {
@@ -45,35 +58,217 @@ export function useBoardsDispatch() {
 }
 
 function boardsReducer(boards: Board[], action: BoardAction) {
-  const { type, payload } = action;
-  switch (type) {
-    case "ADD": {
-      return [
-        ...boards,
-        {
-          id: payload.id,
-          name: payload.name,
-          columns: [],
-        },
-      ];
+  switch (action.type) {
+    case ActionKind.ADD_BOARD: {
+      const newBoard: Board = {
+        id: uuid(),
+        name: action.boardName || "New Board",
+        columns: [],
+      };
+      return [...boards, newBoard];
     }
-    case "CHANGE": {
-      return boards.map((t) => {
-        if (t.id === payload.id) {
-          if (!payload.name) {
-            throw Error("Name is required to change a board");
-          }
-          return (t.name = payload.name);
-        } else {
-          return t;
-        }
-      });
+    case ActionKind.CHANGE_BOARD: {
+      const { boardId, boardName } = action;
+      return boards.map((board) =>
+        board.id === boardId ? { ...board, name: boardName } : board
+      );
     }
-    case "DELETE": {
-      return boards.filter((t) => t.id !== payload.id);
+    case ActionKind.DELETE_BOARD: {
+      const { boardId } = action;
+      return boards.filter((board) => board.id !== boardId);
+    }
+    case ActionKind.ADD_COLUMN: {
+      const { boardId, columnName } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: [
+                ...board.columns,
+                { id: uuid(), name: columnName || "New Column", tasks: [] },
+              ],
+            }
+          : board
+      );
+    }
+    case ActionKind.CHANGE_COLUMN: {
+      const { boardId, columnId, columnName } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: board.columns.map((column) =>
+                column.id === columnId
+                  ? { ...column, name: columnName }
+                  : column
+              ),
+            }
+          : board
+      );
+    }
+    case ActionKind.DELETE_COLUMN: {
+      const { boardId, columnId } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: board.columns.filter((column) => column.id !== columnId),
+            }
+          : board
+      );
+    }
+    case ActionKind.ADD_TASK: {
+      const { boardId, columnId, taskName } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: board.columns.map((column) =>
+                column.id === columnId
+                  ? {
+                      ...column,
+                      tasks: [
+                        ...column.tasks,
+                        {
+                          id: uuid(),
+                          name: taskName || "New Task",
+                          subtasks: [],
+                        },
+                      ],
+                    }
+                  : column
+              ),
+            }
+          : board
+      );
+    }
+    case ActionKind.CHANGE_TASK: {
+      const { boardId, columnId, taskId, taskName } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: board.columns.map((column) =>
+                column.id === columnId
+                  ? {
+                      ...column,
+                      tasks: column.tasks.map((task) =>
+                        task.id === taskId ? { ...task, name: taskName } : task
+                      ),
+                    }
+                  : column
+              ),
+            }
+          : board
+      );
+    }
+    case ActionKind.DELETE_TASK: {
+      const { boardId, columnId, taskId } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: board.columns.map((column) =>
+                column.id === columnId
+                  ? {
+                      ...column,
+                      tasks: column.tasks.filter((task) => task.id !== taskId),
+                    }
+                  : column
+              ),
+            }
+          : board
+      );
+    }
+    case ActionKind.ADD_SUBTASK: {
+      const { boardId, columnId, taskId, subtaskName } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: board.columns.map((column) =>
+                column.id === columnId
+                  ? {
+                      ...column,
+                      tasks: column.tasks.map((task) =>
+                        task.id === taskId
+                          ? {
+                              ...task,
+                              subtasks: [
+                                ...task.subtasks,
+                                {
+                                  id: uuid(),
+                                  name: subtaskName || "New Subtask",
+                                },
+                              ],
+                            }
+                          : task
+                      ),
+                    }
+                  : column
+              ),
+            }
+          : board
+      );
+    }
+    case ActionKind.CHANGE_SUBTASK: {
+      const { boardId, columnId, taskId, subtaskId, subtaskName } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: board.columns.map((column) =>
+                column.id === columnId
+                  ? {
+                      ...column,
+                      tasks: column.tasks.map((task) =>
+                        task.id === taskId
+                          ? {
+                              ...task,
+                              subtasks: task.subtasks.map((subtask) =>
+                                subtask.id === subtaskId
+                                  ? { ...subtask, name: subtaskName }
+                                  : subtask
+                              ),
+                            }
+                          : task
+                      ),
+                    }
+                  : column
+              ),
+            }
+          : board
+      );
+    }
+    case ActionKind.DELETE_SUBTASK: {
+      const { boardId, columnId, taskId, subtaskId } = action;
+      return boards.map((board) =>
+        board.id === boardId
+          ? {
+              ...board,
+              columns: board.columns.map((column) =>
+                column.id === columnId
+                  ? {
+                      ...column,
+                      tasks: column.tasks.map((task) =>
+                        task.id === taskId
+                          ? {
+                              ...task,
+                              subtasks: task.subtasks.filter(
+                                (subtask) => subtask.id !== subtaskId
+                              ),
+                            }
+                          : task
+                      ),
+                    }
+                  : column
+              ),
+            }
+          : board
+      );
     }
     default: {
-      throw Error("Unknown  " + type);
+      throw new Error(`Unhandled action type: ${action.type as string}`);
     }
   }
 }
