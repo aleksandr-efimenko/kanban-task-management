0b120003bd16f3f7e4470e1547b00cad276c2864
androidblog <androidblog@yandex.ru>
2023-06-01 17:01:14 +0400

    add styles to global modal window

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,3 +1,22 @@
-export default function ModalWindow() {
-  return <div className="absolute inset-0 "></div>;
+export default function ModalWindow({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const handleCloseModal = () => {
+    console.log("Close modal");
+  };
+  return (
+    <div
+      className="absolute inset-0 z-50 bg-black bg-opacity-50"
+      onClick={handleCloseModal}
+    >
+      <div
+        className="absolute left-1/2
+      top-1/2 w-[30rem] -translate-x-1/2 -translate-y-1/2 transform rounded-md bg-white"
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
