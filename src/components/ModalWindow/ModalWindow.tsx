af766f1f70ec392c3cf8d5c5a11b668f25f9646d
androidblog <androidblog@yandex.ru>
2023-06-01 21:24:36 +0400

    improve modal window styles

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,22 +1,30 @@
 export default function ModalWindow({
+  modalWindowOpen,
+  setModalWindowOpen,
   children,
 }: {
+  modalWindowOpen: boolean;
+  setModalWindowOpen: React.Dispatch<React.SetStateAction<boolean>>;
   children: React.ReactNode;
 }) {
   const handleCloseModal = () => {
-    console.log("Close modal");
+    setModalWindowOpen(false);
   };
   return (
-    <div
-      className="absolute inset-0 z-50 bg-black bg-opacity-50"
-      onClick={handleCloseModal}
-    >
+    { modalWindowOpen } && (
       <div
-        className="absolute left-1/2
-      top-1/2 w-[30rem] -translate-x-1/2 -translate-y-1/2 transform rounded-md bg-white"
+        className="absolute inset-0 z-50 bg-black bg-opacity-50"
+        onClick={handleCloseModal}
       >
-        {children}
+        <div
+          className="absolute left-1/2
+      top-1/2 w-[30rem] -translate-x-1/2 -translate-y-1/2 
+      transform rounded-md bg-white p-8
+      dark:bg-dark-gray"
+        >
+          {children}
+        </div>
       </div>
-    </div>
+    )
   );
 }
