00ee2ebd4d4f748c8c1f6ca72c49f54db7846418
androidblog <androidblog@yandex.ru>
2023-06-02 20:38:30 +0400

    add modal window invoke by create new board button

--- src/components/LeftPanel/CreateNewBoardButton.tsx
+++ src/components/LeftPanel/CreateNewBoardButton.tsx
@@ -2,28 +2,19 @@ import { type StaticImageData } from "next/image";
 import Link from "next/link";
 import boardIcon from "~/assets/icon-board.svg";
 import Image from "next/image";
-import { useBoardsDispatch } from "@/context/BoardsContext";
-import { ActionKind } from "@/context/BoardsContext";
+import { AddBoard } from "../ModalWindow/AddBoard";
+import { useContext } from "react";
+import { ModalContext } from "@/context/ModalContext";
 
-export function CreateNewBoardButton({
-  handleClick,
-}: {
-  handleClick?: () => void;
-}) {
-  const dispatch = useBoardsDispatch();
-  console.log(new Date());
+export function CreateNewBoardButton() {
+  const { handleModal } = useContext(ModalContext);
   return (
     <Link
       href="#"
       className={`group flex min-h-[3rem] w-full items-center gap-4 
         overflow-hidden text-ellipsis rounded-r-full pl-8 text-purple duration-200
         hover:scale-105`}
-      onClick={() =>
-        dispatch({
-          type: ActionKind.ADD_BOARD,
-          boardName: "New Board",
-        })
-      }
+      onClick={() => handleModal(<AddBoard />)}
     >
       <Image
         src={boardIcon as StaticImageData}
