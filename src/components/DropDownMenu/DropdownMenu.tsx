aad0a0cc07a864e11ebbaa582c8055ec549cc450
androidblog <androidblog@yandex.ru>
2023-06-08 21:07:45 +0400

    add task status dropdown menu

--- src/components/DropDownMenu/DropdownMenu.tsx
+++ src/components/DropDownMenu/DropdownMenu.tsx
@@ -7,30 +7,59 @@ import {
   DropdownDeleteTaskItem,
   DropdownEditTaskItem,
 } from "./DropdownMenuTask";
+import { DropdownMenuItem } from "./DropdownMenuItem";
+import { DropdownMenuContext } from "@/context/DrowdownMenuContext";
+import { useContext } from "react";
 
-export function DropdownMenu({ buttons }: { buttons: JSX.Element[] }) {
+export function DropdownMenu({
+  buttons,
+  position,
+}: {
+  buttons: JSX.Element[];
+  position?: string;
+}) {
   return (
     <div
-      className="absolute -left-[11.5rem] top-14 flex w-48 flex-col rounded-lg bg-white 
-    py-4 dark:bg-very-dark-gray"
+      className={`absolute flex flex-col rounded-lg bg-white 
+    py-4 dark:bg-very-dark-gray ${position ? position : "right-0"}`}
     >
       {buttons}
     </div>
   );
 }
 
-export function BoardDropDownMenu({ boardId }: { boardId: string }) {
+export function BoardDropdownMenu({ boardId }: { boardId: string }) {
   const buttons = [
     <DropdownEditBoardItem boardId={boardId} key={uuid()} />,
     <DropdownDeleteBoardItem boardId={boardId} key={uuid()} />,
   ];
-  return <DropdownMenu buttons={buttons} />;
+  return (
+    <DropdownMenu position="-left-[11.5rem] top-14 w-48" buttons={buttons} />
+  );
 }
 
-export function TaskDropDownMenu({ taskId }: { taskId: string }) {
+export function TaskDropdownMenu({ taskId }: { taskId: string }) {
   const buttons = [
     <DropdownEditTaskItem taskId={taskId} key={uuid()} />,
     <DropdownDeleteTaskItem taskId={taskId} key={uuid()} />,
   ];
   return <DropdownMenu buttons={buttons} />;
+}
+
+export function TaskStatuskDropdownMenu({
+  options,
+  handleClick,
+}: {
+  options: string[];
+  handleClick: (value: string) => void;
+}) {
+  const buttons = options.map((option) => (
+    <DropdownMenuItem
+      onClick={() => handleClick(option)}
+      title={option}
+      destructive={false}
+      key={uuid()}
+    />
+  ));
+  return <DropdownMenu buttons={buttons} position="w-full" />;
 }
