93769a222a2745ddf703d8f3a30f8decd64a0225
androidblog <androidblog@yandex.ru>
2023-06-09 21:12:50 +0400

    create functions for getting board data

---
+++ src/utils/getBoardData.ts
@@ -0,0 +1,54 @@
+import { type Task, type Board } from "@/utils/DataTypes";
+
+export function getColumnIdByTaskId(
+  taskId: string,
+  boards: Board[]
+): string | undefined {
+  const board = boards.find((board) =>
+    board.columns.find((column) =>
+      column.tasks.find((task) => task.id === taskId)
+    )
+  );
+  const column = board?.columns.find((column) =>
+    column.tasks.find((task) => task.id === taskId)
+  );
+
+  return column?.id;
+}
+
+export function getBoardIdByTaskId(
+  taskId: string,
+  boards: Board[]
+): string | undefined {
+  const board = boards.find((board) =>
+    board.columns.find((column) =>
+      column.tasks.find((task) => task.id === taskId)
+    )
+  );
+  return board?.id;
+}
+
+export function getTaskInfoFromId(
+  taskId: string,
+  boards: Board[]
+): Task | undefined {
+  const taskBoardId = getBoardIdByTaskId(taskId, boards);
+  const taskColumnId = getColumnIdByTaskId(taskId, boards);
+  const task = boards
+    .find((board) => board.id === taskBoardId)
+    ?.columns.find((column) => column.id === taskColumnId)
+    ?.tasks.find((task) => task.id === taskId);
+  return task;
+}
+
+export function getBoardDataFromTaskId(
+  taskId: string,
+  boards: Board[]
+): Board | undefined {
+  const board = boards.find((board) =>
+    board.columns.find((column) =>
+      column.tasks.find((task) => task.id === taskId)
+    )
+  );
+  return board;
+}
