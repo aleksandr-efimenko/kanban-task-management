6dac4b97fb97b422fc7db3af21f2930801ab314d
androidblog <androidblog@yandex.ru>
2023-06-09 14:16:12 +0400

    implement ability to open info by click on task card

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,16 +1,29 @@
+import { ModalContext } from "@/context/ModalContext";
 import { type Subtask } from "@/utils/DataTypes";
+import { useContext } from "react";
+import { TaskView } from "./ModalWindow/TaskView";
 
 export function TaskCard({
+  taskId,
   title,
   subtasks,
 }: {
+  taskId: string;
   title: string;
   subtasks: Subtask[];
 }) {
+  const { handleModal } = useContext(ModalContext);
+  const handleOpenTask = () => {
+    handleModal(<TaskView taskId={taskId} />);
+  };
   const subtasksNumber = subtasks.length;
   const subtasksDone = subtasks.filter((subtask) => subtask.isCompleted).length;
   return (
-    <div className="rounded-lg bg-white px-4 py-6 shadow-task-card-shadow dark:bg-dark-gray">
+    <div
+      className="cursor-pointer rounded-lg bg-white px-4 py-6 shadow-task-card-shadow 
+    duration-200 hover:bg-purple hover:bg-opacity-20 dark:bg-dark-gray  dark:hover:bg-purple dark:hover:bg-opacity-20"
+      onClick={handleOpenTask}
+    >
       <h3 className="text-heading-m">{title}</h3>
       <p className="mt-2 text-body-m text-medium-gray">
         {subtasksDone} of {subtasksNumber} subtasks
