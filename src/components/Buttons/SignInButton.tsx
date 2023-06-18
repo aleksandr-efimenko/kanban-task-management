4b0e7da87f2208cd05a4aa63f0ab81ddfab95f35
Aleksandr Efimenko <androidblog@yandex.ru>
2023-06-18 22:25:01 +0400

    created sign in button

---
+++ src/components/Buttons/SignInButton.tsx
@@ -0,0 +1,28 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import { ButtonPrimaryS } from "./MainButtons";
+
+export function SignInButton() {
+  const { data: session } = useSession();
+  const getAuthInfo = () => {
+    if (session) {
+      return (
+        <>
+          <p>Signed in as {session.user.name}</p>
+          <ButtonPrimaryS onClick={() => signOut()}>Sign out</ButtonPrimaryS>
+        </>
+      );
+    }
+    return (
+      <>
+        <p className="w-64">Not signed in</p>
+        <ButtonPrimaryS onClick={() => signIn()}>Sign in</ButtonPrimaryS>
+      </>
+    );
+  };
+
+  return (
+    <div className="flex items-center justify-center gap-3">
+      {getAuthInfo()}
+    </div>
+  );
+}
