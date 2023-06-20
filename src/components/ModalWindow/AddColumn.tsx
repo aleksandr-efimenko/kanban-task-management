984a17cc2d5323da9c96fd3ac9f785de6329dd49
Aleksandr Efimenko <androidblog@yandex.ru>
2023-06-20 17:20:22 +0400

    implement create new column to db

--- src/components/ModalWindow/AddColumn.tsx
+++ src/components/ModalWindow/AddColumn.tsx
@@ -4,24 +4,33 @@ import { useContext, useState } from "react";
 import { ButtonPrimaryS } from "../Buttons/MainButtons";
 import { LabelledTextField } from "../Inputs/LabelledTextField";
 import { ModalWindowTitle } from "./ModalWindowTitle";
+import { api } from "@/utils/api";
 
 export function AddColumn({ boardId }: { boardId: string }) {
   const dispatch = useBoardsDispatch();
   const { handleModal } = useContext(ModalContext);
   const [columnName, setColumnName] = useState("");
   const [errorMesage, setErrorMessage] = useState("");
+  const createColumnMutation = api.columns.createColumn.useMutation();
 
-  const handleAddColumn = () => {
+  const handleAddColumn = async () => {
     if (columnName === "") {
       setErrorMessage("Can't be empty");
       return;
     }
 
     if (!dispatch) return;
+    const newColumn = await createColumnMutation.mutateAsync({
+      boardId: boardId,
+      name: columnName,
+    });
+
     dispatch({
       type: "ADD_COLUMN",
       boardId: boardId,
       columnName: columnName,
+      newColumnId: newColumn.id,
+      color: newColumn.color || "blue",
     });
     setColumnName("");
     handleModal();
@@ -43,7 +52,11 @@ export function AddColumn({ boardId }: { boardId: string }) {
           inputType="textInput"
         />
 
-        <ButtonPrimaryS onClick={handleAddColumn}>
+        <ButtonPrimaryS
+          onClick={() => {
+            void handleAddColumn();
+          }}
+        >
           Create New Column
         </ButtonPrimaryS>
       </>
